f9c7dfa4d9d614a89c15a4ab0815ff499fa30d91
vieraboschkova <58532638+vieraboschkova@users.noreply.github.com>
2020-09-04 09:29:27 +0600

    feat: add interface structure for search response

--- src/app/models/search-response.model.ts
+++ src/app/models/search-response.model.ts
@@ -1,2 +1,13 @@
-export class SearchResponse {
+import  { ISearchItem } from './search-item.model'
+
+export interface ISearchResponse {
+    kind: string;
+    etag: string;
+    pageInfo: IPageInfo;
+    items: ISearchItem[]
+}
+
+interface IPageInfo {
+    totalResults: number;
+    resultsPerPage: number;
 }
