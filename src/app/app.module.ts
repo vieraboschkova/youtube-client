8ebea920dbeeae2c61f09eb0578a53541cd95f57
vieraboschkova <58532638+vieraboschkova@users.noreply.github.com>
2020-09-04 08:57:54 +0600

    feat: add detailed components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SearchResultsComponent } from './components/search-results/search-results.component';
 import { SearchItemComponent } from './components/search-item/search-item.component';
+import { SearchFormComponent } from './components/search-form/search-form.component';
+import { SearchFilterComponent } from './components/search-filter/search-filter.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SearchResultsComponent,
-    SearchItemComponent
+    SearchItemComponent,
+    SearchFormComponent,
+    SearchFilterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
