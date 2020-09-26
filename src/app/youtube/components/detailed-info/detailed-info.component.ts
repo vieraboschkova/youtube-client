31565ec7a146af0e4b4380d44c2bda6c21ec16fe
vieraboschkova <58532638+vieraboschkova@users.noreply.github.com>
2020-09-26 19:36:23 +0600

    feat: add styling to detailed-info component

--- src/app/youtube/components/detailed-info/detailed-info.component.ts
+++ src/app/youtube/components/detailed-info/detailed-info.component.ts
@@ -3,12 +3,18 @@ import { ActivatedRoute, Params } from '@angular/router';
 import { ISearchItem } from '../../models/search-item.model';
 import { ISearchResponse } from '../../models/search-response.model';
 import { response } from '../search-results/response.module';
+import {faEye, faHeart, faHeartBroken, faCommentAlt, IconDefinition} from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-detailed-info',
   templateUrl: './detailed-info.component.html',
   styleUrls: ['./detailed-info.component.scss']
 })
 export class DetailedInfoComponent implements OnInit {
+  public faEye: IconDefinition = faEye;
+  public faHeart: IconDefinition = faHeart;
+  public faHeartBroken: IconDefinition = faHeartBroken;
+  public faCommentAlt: IconDefinition = faCommentAlt;
   items: ISearchItem[] = response.items;
   // itemId: {id: string};
   currentItem: ISearchItem[];
